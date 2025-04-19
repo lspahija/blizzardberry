d4fbbddbc93059e2c663259462e853d106419994
lspahija <lspahijaoec@gmail.com>
2025-04-19 17:36:00 -0500

    multi step tool call

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,7 +1,7 @@
 import { createOpenAICompatible } from '@ai-sdk/openai-compatible';
-import { streamText } from 'ai';
+import { streamText, tool } from 'ai';
+import { z } from 'zod';
 
-export const maxDuration = 30;
 const lmstudio = createOpenAICompatible({
     name: 'lmstudio',
     baseURL: process.env.LMSTUDIO_BASE_URL!,
@@ -13,6 +13,35 @@ export async function POST(req: Request) {
     const result = streamText({
         model: lmstudio('gemma-3-4b-it-qat'),
         messages,
+        tools: {
+            weather: tool({
+                description: 'Get the weather in a location (fahrenheit)',
+                parameters: z.object({
+                    location: z.string().describe('The location to get the weather for'),
+                }),
+                execute: async ({ location }) => {
+                    const temperature = Math.round(Math.random() * (90 - 32) + 32);
+                    return {
+                        location,
+                        temperature,
+                    };
+                },
+            }),
+            convertFahrenheitToCelsius: tool({
+                description: 'Convert a temperature in fahrenheit to celsius',
+                parameters: z.object({
+                    temperature: z
+                        .number()
+                        .describe('The temperature in fahrenheit to convert'),
+                }),
+                execute: async ({ temperature }) => {
+                    const celsius = Math.round((temperature - 32) * (5 / 9));
+                    return {
+                        celsius,
+                    };
+                },
+            }),
+        },
     });
 
     return result.toDataStreamResponse();
