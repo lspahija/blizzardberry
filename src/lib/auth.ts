e8abf1343d84fa16b057f3e17c01937996cf03b7
lspahija <lspahijaoec@gmail.com>
2025-05-17 20:08:24 -0500

    redirecting to dashboard upon sign in

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -3,4 +3,15 @@ import GitHub from "next-auth/providers/github"
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
     providers: [GitHub],
+    callbacks: {
+        async redirect({ url, baseUrl }) {
+            return baseUrl + "/dashboard";
+        },
+    },
 })
+
+export { auth as middleware }
+
+export const config = {
+    matcher: ["/dashboard"],
+};
