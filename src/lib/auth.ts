07f28122311f9ddcbabb7dacd95bb04765e733bb
lspahija <lspahijaoec@gmail.com>
2025-05-22 23:04:59 -0500

    next auth storing in db successfully

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,9 +1,14 @@
 import NextAuth from "next-auth"
 import GitHub from "next-auth/providers/github"
 import Google from "next-auth/providers/google"
+import {SupabaseAdapter} from "@auth/supabase-adapter";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
     providers: [GitHub, Google],
+    adapter: SupabaseAdapter({
+        url: process.env.SUPABASE_URL,
+        secret: process.env.SUPABASE_SERVICE_ROLE_KEY,
+    }),
     callbacks: {
         async redirect({ url, baseUrl }) {
             return baseUrl + "/dashboard";
