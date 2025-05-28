9fb911de46f28250d87d7b66654dcbe0b13be526
lspahija <lspahijaoec@gmail.com>
2025-05-28 11:39:08 -0500

    fixed auth.ts

--- src/lib/auth/auth.ts
+++ src/lib/auth/auth.ts
@@ -1,13 +1,22 @@
 import NextAuth from 'next-auth';
 import Resend from 'next-auth/providers/resend';
+import GitHub from 'next-auth/providers/github';
+import Google from 'next-auth/providers/google';
+import { SupabaseAdapter } from '@auth/supabase-adapter';
 import { sendVerificationRequest } from './email';
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   providers: [
+    GitHub,
+    Google,
     Resend({
       apiKey: process.env.AUTH_RESEND_KEY,
       from: process.env.AUTH_EMAIL_FROM,
       sendVerificationRequest,
     }),
   ],
+  adapter: SupabaseAdapter({
+    url: process.env.SUPABASE_URL,
+    secret: process.env.SUPABASE_SERVICE_ROLE_KEY,
+  }),
 });
