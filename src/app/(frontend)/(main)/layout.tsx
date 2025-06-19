a12aba3e1c0596ca52576e333c384074136a2ab9
Frane Spahija <113150571+fspah@users.noreply.github.com>
2025-06-19 14:27:00 +0200

    Is loggedin (#34)
    * check if user is loggedin for pricing
    
    * stripe popup fix
    
    * remove debug
    
    * remove unusued navbar
    
    * switch navbar to page.tsx pricing

--- src/app/(frontend)/(main)/layout.tsx
+++ src/app/(frontend)/(main)/layout.tsx
@@ -1,38 +1,28 @@
 import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
-import '../globals.css';
 import { SessionProvider } from 'next-auth/react';
 import { FrameworkProvider } from '@/app/(frontend)/contexts/useFramework';
-
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-});
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
-});
+import { Suspense } from 'react';
+import { AuthProvider } from '@/app/context/AuthContext';
 
 export const metadata: Metadata = {
   title: 'BlizzardBerry',
   description: 'Building the Future of UX',
 };
 
-export default function RootLayout({
+export default function FrontendLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <SessionProvider>
-          <FrameworkProvider>{children}</FrameworkProvider>
-        </SessionProvider>
-      </body>
-    </html>
+    <Suspense fallback={null}>
+      <SessionProvider>
+        <AuthProvider>
+          <FrameworkProvider>
+            {children}
+          </FrameworkProvider>
+        </AuthProvider>
+      </SessionProvider>
+    </Suspense>
   );
 }
--- src/app/(frontend)/(main)/pricing/layout.tsx
+++ src/app/(frontend)/(main)/pricing/layout.tsx
@@ -1,14 +1,7 @@
-import { Navbar } from '@/app/(frontend)/components/Navbar';
-
 export default function PricingLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return (
-    <>
-      <Navbar />
-      {children}
-    </>
-  );
+  return <>{children}</>;
 }
