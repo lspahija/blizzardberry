b79914daa175d1749dd79ed2afb91a601e2189c0
fspah <franespahija8@gmail.com>
2025-07-04 23:09:52 +0200

    fix

--- public/agent/agent.js
+++ public/agent/agent.js
@@ -224,7 +224,7 @@
         parts: [
           {
             type: 'text',
-            text: `✅ ${actionModel.toolName?.replace(/^ACTION_(CLIENT_|SERVER_)/, '') || actionModel.action || 'Action'} successfully executed.)}`,
+            text: `✅ ${actionModel.toolName?.replace(/^ACTION_(CLIENT_|SERVER_)/, '') || actionModel.action || 'Action'} successfully executed.`,
           },
         ],
       });
