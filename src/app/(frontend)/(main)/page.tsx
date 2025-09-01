339a8fe56712f7eaf4b2f3d9f4dc80a4fb1af243
Nikolas Stojak <stojak.nikolas@icloud.com>
2025-09-01 12:48:08 +0200

    feat: fine-tune decorative SVG position on landing page
    - Changed SVG position from -top-4 to -top-2
    - Smaller adjustment for better visual balance

--- src/app/(frontend)/(main)/page.tsx
+++ src/app/(frontend)/(main)/page.tsx
@@ -169,7 +169,7 @@ export default function LandingPage() {
               <div className="relative w-full max-w-lg lg:max-w-none">
                 {/* Decorative SVG for right side */}
                 <svg
-                  className="hidden lg:block absolute w-16 lg:w-20 h-auto flex-shrink-0 -top-4 right-0 translate-x-4 lg:translate-x-8"
+                  className="hidden lg:block absolute w-16 lg:w-20 h-auto flex-shrink-0 -top-2 right-0 translate-x-4 lg:translate-x-8"
                   viewBox="0 0 92 80"
                   xmlns="http://www.w3.org/2000/svg"
                 >
