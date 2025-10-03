84eac5e132d6f6b0af5e56a2289ff561d4056fbd
lspahija <lspahijaoec@gmail.com>
2025-10-03 03:15:23 -0500

    useful queries

--- sql/useful-queries.sql
+++ sql/useful-queries.sql
@@ -1,37 +1,18 @@
-select * from agents;
-select * from actions;
-select * from documents;
-
-select * from next_auth.accounts;
-select * from next_auth.users;
-
-
-delete FROM agents;
-delete from actions;
-delete from documents;
-
-drop table if exists actions;
-drop type if exists execution_context;
-drop table if exists documents;
-drop table if exists agents;
-drop function if exists search_documents;
-
-drop table if exists domain_events;
-drop table if exists credit_holds;
-drop materialized view user_credit_summary;
-drop table if exists credit_batches;
-
-
+-- add credits to a user
 insert into credit_batches(user_id, quantity_remaining)
 values ('83d476b1-1777-4b1d-8f07-28d7df584f25', 1000000);
 
+-- make some user pro tier
+update subscriptions set tier = 'pro' where user_id = 'f3352267-77a4-45d7-a9c4-155afbc55979';
 
-drop table subscriptions;
-delete from domain_events;
-delete from subscriptions;
-
-select * from subscriptions;
-
-
-
-select * from subscriptions;
+-- copy actions from one agent to another
+INSERT INTO actions (agent_id, created_at, execution_model, name, description, execution_context)
+SELECT
+    'b53516b4-48d8-4563-8059-ead022a794f0'::uuid,  -- destination agent
+    created_at,
+    execution_model,
+    name,
+    description,
+    execution_context
+FROM actions
+WHERE agent_id = '6a2ae59c-26da-4b01-913d-f014ea7cdbd9'; -- source agent
