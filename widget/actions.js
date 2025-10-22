dec48839b90f8d6cdd6fe7aee74ee0bbb2698f9b
lspahija <lspahijaoec@gmail.com>
2025-10-21 20:32:32 -0500

    removed pointless try catch

--- widget/actions.js
+++ widget/actions.js
@@ -6,33 +6,29 @@ import { config } from './config';
 export async function executeAction(actionModel) {
   console.log('executing action: ', actionModel);
 
-  try {
-    const actionResult = actionModel.toolName.startsWith('ACTION_CLIENT_')
-      ? await executeClientAction(actionModel)
-      : await executeServerAction(actionModel);
+  const actionResult = actionModel.toolName.startsWith('ACTION_CLIENT_')
+    ? await executeClientAction(actionModel)
+    : await executeServerAction(actionModel);
 
-    const actionName = actionModel.toolName.replace(
-      /^ACTION_(CLIENT_|SERVER_)/,
-      ''
-    );
+  const actionName = actionModel.toolName.replace(
+    /^ACTION_(CLIENT_|SERVER_)/,
+    ''
+  );
 
-    state.messages.push({
-      id: generateId(),
-      role: 'assistant',
-      parts: [
-        {
-          type: 'text',
-          text: `The action ${actionName} was successfully executed.`,
-        },
-      ],
-    });
+  state.messages.push({
+    id: generateId(),
+    role: 'assistant',
+    parts: [
+      {
+        type: 'text',
+        text: `The action ${actionName} was successfully executed.`,
+      },
+    ],
+  });
 
-    await persistMessage(state.messages[state.messages.length - 1]);
+  await persistMessage(state.messages[state.messages.length - 1]);
 
-    return `ACTION_RESULT: ${JSON.stringify(actionResult)}`;
-  } catch (error) {
-    throw error;
-  }
+  return `ACTION_RESULT: ${JSON.stringify(actionResult)}`;
 }
 
 async function executeClientAction(actionModel) {
